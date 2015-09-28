18b582e3be67d880dd9b6d7a55b2c06440100ba1
Justin Kim <yulli@yulli.org>
2015-09-28 12:24:36 -0400

    Add OptionsValid

--- util.go
+++ util.go
@@ -9,3 +9,26 @@ func OptionExists(target string) bool {
 
 	return false
 }
+
+func OptionsValid(av *Avalon) bool {
+	// No Lake until 7 players
+	if av.EnabledOptions["lake"] {
+		if av.NumPlayers() < 7 {
+			return false
+		}
+	}
+
+	// No Oberon until 10 players
+	if av.EnabledOptions["oberon"] {
+		if av.NumPlayers() < 10 {
+			return false
+		}
+	}
+
+	// Keep at least one evil for Assassin
+	if av.NumEvilSpecials() >= av.NumEvils() {
+		return false
+	}
+
+	return true
+}
--- util_test.go
+++ util_test.go
@@ -23,3 +23,66 @@ func TestOptionExists(t *testing.T) {
 		}
 	}
 }
+
+// * Lake requires 7 players
+// * Oberon requires 10 players
+// * Must reserve at least one evil slot for assassin
+func TestOptionsValid(t *testing.T) {
+	var tests = []struct {
+		players []string
+		options map[string]bool
+		want    bool
+	}{
+		{
+			[]string{"A", "B", "C", "D", "E", "F"},
+			map[string]bool{"lake": true},
+			false,
+		},
+		{
+			[]string{"A", "B", "C", "D", "E", "F", "G"},
+			map[string]bool{"lake": true},
+			true,
+		},
+		{
+			[]string{"A", "B", "C", "D", "E", "F", "G", "H", "I"},
+			map[string]bool{"oberon": true},
+			false,
+		},
+		{
+			[]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"},
+			map[string]bool{"oberon": true},
+			true,
+		},
+		{
+			[]string{"A", "B", "C", "D", "E", "F"},
+			map[string]bool{"mordred": true, "morganapercival": true},
+			false,
+		},
+		{
+			[]string{"A", "B", "C", "D", "E", "F", "G"},
+			map[string]bool{"mordred": true, "morganapercival": true},
+			true,
+		},
+		{
+			[]string{"A", "B", "C", "D", "E", "F", "G", "H", "I"},
+			map[string]bool{"mordred": true, "morganapercival": true},
+			true,
+		},
+		{
+			[]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"},
+			map[string]bool{"mordred": true, "morganapercival": true, "oberon": true},
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		av := NewAvalon()
+		av.Players = test.players
+		av.EnabledOptions = test.options
+
+		res := OptionsValid(av)
+		if res != test.want {
+			t.Errorf("expected %t for players %v, options %v, got %t", test.want, test.players, test.options, res)
+		}
+	}
+}
